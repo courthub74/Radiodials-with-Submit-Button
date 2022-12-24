dec9a627abc62d44dc7c94f44bd49f08442e90e6
Courdevelops <courdevelops@gmail.com>
2022-12-23 20:31:39 -0500

    first commit

---
+++ radiodial.js
@@ -0,0 +1,24 @@
+// radio dial js
+
+// get the radio dials through the DOM
+let radio = document.getElementById("radios");
+// test print
+console.log(radio);
+// loop through the radio dials through the parent div
+    // radios
+    // give it an onclick
+radio.addEventListener('click', (e) =>{
+    if(e.target && e.target.id == "one"){
+        // alert("Button 1");
+        // call a function to submit button 
+            // that takes this value and makes an alert with it
+        // get the value of one
+        let one = document.getElementById("one").value;
+        console.log(one);
+    }
+});
+
+// function for 1
+function button1(){
+    alert("Button1");
+}
