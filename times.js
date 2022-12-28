a09c79d16e8e00560fbaf4fbbf10c2aa2d391987
Courdevelops <courdevelops@gmail.com>
2022-12-28 17:04:18 -0500

    this works proper top

--- times.js
+++ times.js
@@ -8,7 +8,7 @@ window.addEventListener('load', () => {
     // query the input field
         // in this case it's the radio dials
     
-    // TIME field
+    // TIME fields
     var timebuttons = document.getElementById("timebuttons");
     // test print these
     console.log('Timebuttons div:', timebuttons);
@@ -31,5 +31,80 @@ window.addEventListener('load', () => {
             // change the text color
             labeltwo.style.color = "rgb(6, 68, 6)";
         }
-    })
-})
+        // if 3pm change the button color
+        if(e.target && e.target.id === "three"){
+            // test print the target
+            console.log('The target id:', e.target.id);
+            // get the radio-tile
+            var tilethree = document.getElementById("tilethree");
+            // test print
+            console.log('Radio Tile 3:', tilethree);
+            // change the background color
+            tilethree.style.backgroundColor = "#ffffff";
+            // get label
+            var labelthree = document.getElementById("labelthree");
+            // test print
+            console.log('Label Three:', labelthree);
+            // change the text color
+            labelthree.style.color = "rgb(6, 68, 6)";
+        }
+    });
+    // FORM field
+    // first you must query each time button
+    // TIME fields
+
+
+    // 2pm
+    // const twopm = document.querySelector("#two");
+
+    // 3pm
+    // const threepm = document.querySelector("#three");
+
+    
+    form.addEventListener('submit', (e) => {
+         // keeps page from refreshing
+         e.preventDefault();
+         // test print the submit button
+        console.log("Submit button pressed");
+        
+        // check for all inputs
+        var timevalue = document.querySelector('input[name="timebutt"]:checked').value;
+
+        // Error handling
+            // if no value entered just alert
+        if (!timevalue) {
+            alert("Please select a time");
+            return;
+        }
+       
+        // FOR STYLING BACK AFTER SUBMIT BUTTON PRESSED
+        // get the radio-tile for 2pm
+        var tile2 = document.getElementById("tiletwo");
+        // change the style back
+        tile2.style.backgroundColor = "rgb(6, 68, 6)";
+        tile2.style.border = "none";
+        // get the label
+        var label2 = document.getElementById("labeltwo");
+        // change the text style back
+        label2.style.color = "#ffffff";
+        
+        // get the radio-tile for 3pm
+        var tile3 = document.getElementById("tilethree");
+        // change the style back
+        tile3.style.backgroundColor = "rgb(6, 68, 6)";
+        tile3.style.border = "none";
+        // get the label
+        var label3 = document.getElementById("labelthree");
+        // change the text style back
+        label3.style.color = "#ffffff";
+
+        // YOU GET THE VALUES BASED ON ALL THE INPUTS
+        // THEY SHOULD GO UNDER THE SAME CLASS OR TAG
+        // CHECK IF ITS CHECKED
+
+        // THIS goes to the database
+        console.log('Time Selected:', timevalue);
+        // change the inner html of the output alert
+        document.getElementById("submitalert").innerHTML = "A time was Submitted";
+    });
+});
